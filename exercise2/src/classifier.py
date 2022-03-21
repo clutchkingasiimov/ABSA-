5c690a4b622ac7975f616f6ed95091c095580721
celineyixinz <73972308+celineyixinz@users.noreply.github.com>
2022-03-21 23:34:33 +0100

    Update classifier.py

--- exercise2/src/classifier.py
+++ exercise2/src/classifier.py
@@ -1,68 +1,22 @@
+import numpy
+import pickle
 
-import pandas as pd
-import numpy as np
-import random
-import time
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchtext import data, datasets
-from sklearn.model_selection import train_test_split
+from utils import * 
+from sklearn.ensemble import RandomForestClassifier
 
-from Utils import load_data,TextProcessor, preprocess,DataFrameDataset,CNN, train_model,evaluate_model,epoch_time,plot_acc,get_predsevaluate
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# set random seed
-SEED = 42
 
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-
-TEXT = data.Field(tokenize = 'spacy',
-                  tokenizer_language = 'en_core_web_sm',
-                  batch_first = True)
-LABEL = data.LabelField(dtype = torch.float)
-MAX_VOCAB_SIZE = 25000
-BATCH_SIZE = 5
-INPUT_DIM = len(TEXT.vocab)
-EMBEDDING_DIM = 100
-N_FILTERS = 200
-
-OUTPUT_DIM = 3  # 3 labels
-DROPOUT = 0.15
-PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
-
-model = CNN(INPUT_DIM, EMBEDDING_DIM, OUTPUT_DIM, N_FILTERS, DROPOUT, PAD_IDX)
-
-# load pre-trained embeddings
-
-pretrained_embeddings = TEXT.vocab.vectors
-model.embedding.weight.data.copy_(pretrained_embeddings)
-
-# zero the initial weights of the unknown and padding tokens
-
-UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
-
-model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
-model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
-
-# Parameters for model training
-LR = 1e-4
-optimizer = optim.Adam(model.parameters(), lr=LR)
-
-criterion = nn.CrossEntropyLoss()
-
-model = model.to(device)
-criterion = criterion.to(device)
-
-N_EPOCHS = 10
-
+embedding = 'BERT'
+bert_version = 'WORD'
 
 
 class Classifier:
     """The Classifier"""
+
+    def __init__(self, n_estimators=100, random_state=42):
+        
+        self.n_estimators = n_estimators
+        self.random_state = random_state 
+        
 
 
     #############################################
@@ -73,102 +27,54 @@ class Classifier:
          OF MODEL HYPERPARAMETERS
         """
         train_set = load_data(trainfile)
+        train_set['clean_sentence_bert'] = train_set['sentence'].apply(cleanText)
+        
+        # get BERT embedding
 
+        
+        
+        
+        # generate featrues 
+        bert_word_training_features = train_set['clean_sentence_bert'].apply(embeddToBERT)
+        feature = [x for x in bert_word_training_features.transpose()]
+        bert_word_training_features = np.asarray(feature)
+        
+        
+        model = RandomForestClassifier(self.n_estimators, self.random_state)
 
-        train, val = train_test_split(train_set, test_size=0.1)
-        train_cleaned = preprocess(train)
-        val_cleaned = preprocess(val)
-        train_ds = DataFrameDataset.splits(
-            text_field=TEXT, label_field=LABEL,
-            df=train_cleaned)
-        train_ds = train_ds[0]
+        # fit the model
+        self.model.fit(bert_word_training_features, 
+                       train_set["polarity_2"])
 
-        val_ds = DataFrameDataset.splits(
-            text_field=TEXT, label_field=LABEL,
-            df=val_cleaned)
-        val_ds = val_ds[0]
+        # save the fitted model
+        pickle.dump(self.model, open('RF_model', 'wb'))
+        
 
-
-
-
-        TEXT.build_vocab(train_ds,
-                         max_size=MAX_VOCAB_SIZE,
-                         vectors="glove.6B.100d",
-                         unk_init=torch.Tensor.normal_)
-
-        LABEL.build_vocab(train_ds)
-
-        # Build iterator
-        train_iterator = data.BucketIterator(
-            (train_ds),
-            batch_size=BATCH_SIZE,
-            sort_key=train_ds.sort_key,
-            device=device)
-
-        val_iterator = data.BucketIterator(
-            (val_ds),
-            batch_size=BATCH_SIZE,
-            sort_key=val_ds.sort_key,
-            device=device)
-
-
-
-        best_valid_loss = float('inf')
-        total_train_acc = []
-        total_val_acc = []
-
-        for epoch in range(N_EPOCHS):
-
-            start_time = time.time()
-
-            train_loss, train_acc = train_model(model, train_iterator, optimizer, criterion)
-            valid_loss, valid_acc = evaluate_model(model, val_iterator, criterion)
-
-            end_time = time.time()
-            epoch_mins, epoch_secs = epoch_time(start_time, end_time)
-
-            total_train_acc.append(train_acc)
-            total_val_acc.append(valid_acc)
-
-            if valid_loss < best_valid_loss:
-                best_valid_loss = valid_loss
-                torch.save(model.state_dict(), 'model.pt')
-
-            print(f'Epoch: {epoch + 1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
-            print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc * 100:.2f}%')
-            print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc * 100:.2f}%')
-
-        plot_acc(total_train_acc, total_val_acc, N_EPOCHS)
 
     def predict(self, datafile):
         """Predicts class labels for the input instances in file 'datafile'
         Returns the list of predicted labels
         """
-        test_cleaned = preprocess(load_data(datafile))
-        test_ds = DataFrameDataset.splits(
-            text_field=TEXT, label_field=LABEL,
-            df=test_cleaned)
-        test_ds = test_ds[0]
+        test_set = load_data(trainfile)
+        test_set['clean_sentence_bert'] = test_set['sentence'].apply(cleanText)
+    
+        
+        # generate featrues 
+        bert_word_test_features = test_set['clean_sentence_bert'].apply(embeddToBERT)
+        feature = [x for x in bert_word_test_features.transpose()]
+        bert_word_test_features = np.asarray(feature)
+        
+        
+        # load model
+        loaded_model = pickle.load(open('RF_model', 'rb'))
+        
+        # predict
+        y_pred_bert_words_rf = loaded_model.predict(bert_word_test_features)
+        
+        # convert numerical labels to text labels 
+        label_dict = {0: 'positive', 1: 'negative', 2: 'neutral'}
+        slabels = [label_dict[i] for i in y_pred_bert_words_rf]
+        
+        return slabels
 
-        # Build iterator
-        test_iterator = data.BucketIterator(
-            (test_ds),
-            batch_size=BATCH_SIZE,
-            sort_key=test_ds.sort_key,
-            device=device)
-
-        model.load_state_dict(torch.load('model.pt'))
-
-        test_loss, test_acc = evaluate_model(model, test_iterator, criterion)
-
-        print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
-
-        return get_predsevaluate(model, test_iterator)
-
-
-
-
-
-
-
-
+        
