68bd74769dee3a7ddc510155ff3a9ab27664e8cd
celineyixinz <celine020510@outlook.com>
2022-03-21 23:33:09 +0100

    Update Utils.py

--- exercise2/src/Utils.py
+++ exercise2/src/Utils.py
@@ -1,31 +1,22 @@
 import pandas as pd
 import string
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from typing import Text
 import re
 import nltk
-import spacy
-
-import nltk
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context
-
-
 nltk.download('punkt')
 nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+from nltk.tokenize import sent_tokenize, word_tokenize 
 from nltk.corpus import stopwords
-import sklearn
+from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import wordnet
+from nltk.tag import pos_tag
 
-from torchtext import data
+import itertools
 
+import mxnet as mx
+from bert_embedding import BertEmbedding
 
 
 def load_data(path):
@@ -37,233 +28,106 @@ def load_data(path):
     3:'char_offset',
     4:'sentence'
 })
+  df['polarity_2'] = df['polarity'].map({'positive':0,'negative':1,'neutral':2})
+  
   return df
 
-class TextProcessor:
-
-    def __init__(self, text):
-      self.text = text
-      self.stopwords = stopwords.words('english')
-      # Keeping the special characters, we re-format the punctuation
-      self.special_chars = re.compile('[{}]'.format(re.escape(string.punctuation)))
-      self.nlp = spacy.load("en_core_web_sm")
-      self.processed_text = []
-
-    def _iterator(self):
-      for text in self.text:
-        yield text
-
-    def Preprocess_Text(self):
-      for text in self._iterator():
-        # Remove numbers from the string
-        cleaned_text = re.sub(r'\d+', '', text)
-
-        # Token creation & Lemmatization
-        doc = self.nlp(cleaned_text)
-        tokens = [token.lemma_ for token in doc]
-        tokens = [token.strip().lower() for token in tokens]
-
-        # Stopword & Punctuation Removal
-        cleaned_tokens = [token for token in tokens if token not in self.stopwords]
-        token_filters = filter(None, [self.special_chars.sub(' ', token) for token in cleaned_tokens])
-        new_text = ' '.join(token_filters)
-
-        # Whitespace removal between pre-existing punctuations & stopwords
-        new_text = " ".join(new_text.split())
-        self.processed_text.append(new_text)
-      return self.processed_text
 
-
-def preprocess(df):
-  """To concatenate aspect category, target_term and original sentence, which will be the input for embedding"""
-
-  tp = TextProcessor(df['sentence'])
-  df['sentence'] = tp.Preprocess_Text()
-  df['inputs'] = df['aspect_cat'] + " " + df['target_term'] + " " + df['sentence']
-
-  return df[['polarity', 'inputs']]
-
-
-
-class DataFrameDataset(torch.legacy.data.Dataset):
-  '''
-  credit: https://gist.github.com/lextoumbourou/8f90313cbc3598ffbabeeaa1741a11c8
-  To create a dataloader suitable for pytorch, we need to transform a dataframe into an iterator
-  '''
-
-  def __init__(self, df, text_field, label_field, is_test=False):
-    fields = [('text', text_field), ('label', label_field)]
-    examples = []
-    for i, row in df.iterrows():
-      label = row.polarity if not is_test else None
-      text = row.inputs
-      examples.append(data.Example.fromlist([text, label], fields))
-
-    super().__init__(examples, fields)
-
-  @staticmethod
-  def sort_key(ex):
-    return len(ex.text)
-
-  @classmethod
-  def splits(cls, text_field, label_field, df):
-    data, data2 = (None, None)
-
-    data = cls(df.copy(), text_field, label_field)
-
-    return tuple(d for d in (data, data2) if d is not None)
-
-# Build the classifier
-class CNN(nn.Module):
-  def __init__(self,
-               vocab_size,
-               embedding_dim,
-               output_dim,
-               n_filters,
-               dropout,
-               pad_idx):
-    super().__init__()
-
-    self.embedding = nn.Embedding(vocab_size, embedding_dim,
-                                  padding_idx=pad_idx)
-
-    # uni-gram
-    self.conv1 = nn.Conv2d(in_channels=1,
-                           out_channels=n_filters,
-                           kernel_size=(1, embedding_dim))
-    # bi-gram
-    self.conv2 = nn.Conv2d(in_channels=1,
-                           out_channels=n_filters,
-                           kernel_size=(2, embedding_dim))
-    # tri-gram
-    self.conv3 = nn.Conv2d(in_channels=1,
-                           out_channels=n_filters,
-                           kernel_size=(3, embedding_dim))
-    # quart-gram
-    self.conv4 = nn.Conv2d(in_channels=1,
-                           out_channels=n_filters,
-                           kernel_size=(4, embedding_dim))
-
-    # (# of conv layers * # of filter, output_dim)
-    self.linear = nn.Linear(4 * n_filters, output_dim)
-
-    self.dropout = nn.Dropout(dropout)
-
-  def forward(self, text):
-    embedded = self.embedding(text)
-
-    embedded = embedded.unsqueeze(1)
-
-    conved1 = F.relu(self.conv1(embedded).squeeze(3))
-    pooled1 = F.max_pool1d(conved1, conved1.shape[2]).squeeze(2)
-
-    conved2 = F.relu(self.conv2(embedded).squeeze(3))
-    pooled2 = F.max_pool1d(conved2, conved2.shape[2]).squeeze(2)
-
-    conved3 = F.relu(self.conv3(embedded).squeeze(3))
-    pooled3 = F.max_pool1d(conved3, conved3.shape[2]).squeeze(2)
-
-    conved4 = F.relu(self.conv4(embedded).squeeze(3))
-    pooled4 = F.max_pool1d(conved4, conved4.shape[2]).squeeze(2)
-
-    concatenated = torch.cat([pooled1, pooled2, pooled3, pooled4], dim=1)
-
-    dropout = self.dropout(concatenated)
-
-    output = self.linear(concatenated)
-
-    return output
-
-
-def accuracy(preds, y):
-  """
-  Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
-  """
-  # #round predictions to the closest integer
-  top_pred = preds.argmax(1, keepdim=True)
-  correct = top_pred.eq(y.view_as(top_pred)).sum()
-  acc = correct.float() / y.shape[0]
-  return acc
-
-def train_model(model, iterator, optimizer, criterion):
-  epoch_loss = 0
-  epoch_acc = 0
-
-  model.train()
-
-  for batch in iterator:
-    optimizer.zero_grad()
-
-    predictions = model(batch.text).squeeze(1)
-
-    loss = criterion(predictions, batch.label.long())
-
-    acc = accuracy(predictions, batch.label)
-
-    loss.backward()
-
-
-    optimizer.step()
-
-    epoch_loss += loss.item()
-    epoch_acc += acc.item()
-
-  return epoch_loss / len(iterator), epoch_acc / len(iterator)
-
-def evaluate_model(model, iterator, criterion):
-
-  epoch_loss = 0
-  epoch_acc = 0
-
-  model.eval()
-
-  with torch.no_grad():
-    for batch in iterator:
-      predictions = model(batch.text).squeeze(1)
-
-      loss = criterion(predictions, batch.label.long())
-
-      acc = accuracy(predictions, batch.label)
-
-      epoch_loss += loss.item()
-      epoch_acc += acc.item()
-
-  return epoch_loss / len(iterator), epoch_acc / len(iterator)
-
-import time
-
-def epoch_time(start_time, end_time):
-    elapsed_time = end_time - start_time
-    elapsed_mins = int(elapsed_time / 60)
-    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
-    return elapsed_mins, elapsed_secs
-import matplotlib.pyplot as plt
-
-def plot_acc(train_acc, val_acc, nb_epochs):
-    plt.plot(list(range(nb_epochs+1))[1:], train_acc)
-    plt.plot(list(range(nb_epochs+1))[1:], val_acc)
-    plt.legend(['train', 'val'])
-    plt.xlabel('Epochs')
-    plt.ylabel('Accuracy')
-    plt.show()
-    # plt.savefig('{}/chart.png'.format(save_folder))
-
-
-def get_predsevaluate(model, iterator):
-  outputs = []
-  pred_labels = []
-
-  model.eval()
-
-  with torch.no_grad():
-
-    for batch in iterator:
-      output = model(batch.text).squeeze(1)
-      outputs.append(output)
-
-    for batch in outputs:
-      for sentence in batch:
-        pred = sentence.argmax()
-        pred_labels.append(pred.item())
-
-  return pred_labels
+def NormalizeWithPOS(text):
+    # Lemmatization & Stemming according to POS tagging
+
+    word_list = word_tokenize(text)
+    rev = []
+    lemmatizer = WordNetLemmatizer() 
+    stemmer = PorterStemmer() 
+    for word, tag in pos_tag(word_list):
+        if tag.startswith('J'):
+            w = lemmatizer.lemmatize(word, pos='a')
+        elif tag.startswith('V'):
+            w = lemmatizer.lemmatize(word, pos='v')
+        elif tag.startswith('N'):
+            w = lemmatizer.lemmatize(word, pos='n')
+        elif tag.startswith('R'):
+            w = lemmatizer.lemmatize(word, pos='r')
+        else:
+            w = word
+        w = stemmer.stem(w)
+        rev.append(w)
+    review = ' '.join(rev)
+    return review
+
+
+def cleanText(text):
+    
+    text = re.sub(r'<.*?>', ' ', text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"n't", " not", text)
+    text = re.sub(r"'ve", " have", text)
+    text = re.sub(r"'ll", " will", text)
+    text = re.sub(r"'re", " are", text)
+
+    # if embedding is not 'BERT':
+    #     text = re.sub(r"[0-9]+", ' ', text)
+    #     text = re.sub(r"-", ' ', text)
+    
+    text = text.strip().lower()
+    
+    
+    # if embedding is not 'BERT':
+    #     # Remove other contractions
+    #     text = re.sub(r"'", ' ', text)
+    
+    # Replace punctuations with space
+    # if embedding is 'BERT': # save ! ? . for end of the sentence detection [,/():;']
+    filters='"#$%&*+<=>@[\\]^_`{|}~\t\n'
+    text = re.sub(r'\!+', '!', text)
+    text = re.sub(r'\?+', '?', text)
+    # else:
+    #     filters='!"\'#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n'
+    # translate_dict = dict((i, " ") for i in filters)
+    # translate_map = str.maketrans(translate_dict)
+    # text = text.translate(translate_map)
+    
+    # if embedding is 'BERT':
+    text = re.sub(r'\( *\)', ' ', text)
+
+    # if embedding is not 'BERT':
+    text = ' '.join([w for w in text.split() if len(w)>1])
+
+    # Replace multiple space with one space
+    text = re.sub(' +', ' ', text)
+    
+    text = ''.join(text)
+
+    return text
+
+
+
+def embeddToBERT(text):
+    
+    ctx = mx.cpu()
+    bert = BertEmbedding(ctx=ctx)
+    
+    sentences = re.split('!|\?|\.',text)
+    sentences = list(filter(None, sentences)) 
+
+    # if bert_version == 'WORD':
+    result = bert(sentences, 'avg') # avg is refer to handle OOV
+
+    bert_vocabs_of_sentence = []
+    for sentence in range(len(result)):
+        for word in range(len(result[sentence][1])):
+            bert_vocabs_of_sentence.append(result[sentence][1][word])
+    feature = [mean(x) for x in zip(*bert_vocabs_of_sentence)]
+
+    # elif bert_version == 'SENTENCE':
+    #     result = bert_transformers.encode(sentences)
+    #     feature = [mean(x) for x in zip(*result)]
+  
+    return feature
+
+
+def mean(z): # used for BERT (word version) and Word2Vec
+    return sum(itertools.chain(z))/len(z)
+    
